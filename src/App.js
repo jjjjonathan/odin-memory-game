d5bdafd0c25fa0749fa695f6aa23fedaf49b0d5e
jjjjonathan <hornathanjon@gmail.com>
2021-03-23 13:15:57 -0500

    add Board

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Board from "./components/Board";
 
 export default function App() {
   return (
     <div>
       <h1>Memory Game</h1>
+      <Board />
     </div>
   );
 }
---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "Card";
+
+export default function Board(props) {
+  return (
+    <div>
+      <Card name="google" />
+      <Card name="facebook" />
+    </div>
+  );
+}
